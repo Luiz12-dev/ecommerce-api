8de79635be4f88b6fe110a82464736f40736ed9c
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-07 14:22:05 -0300

    feat: Adiciona estrutura da entidade Usuario

--- ecommerce-api/src/main/java/br/com/luiz/ecommerce/ecommerce_api/domain/Usuario.java
+++ ecommerce-api/src/main/java/br/com/luiz/ecommerce/ecommerce_api/domain/Usuario.java
@@ -1,5 +1,50 @@
-package br.com.luiz.ecommerce.domain;
+package br.com.luiz.ecommerce.ecommerce_api.domain;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Email
+    private String email;
+
+    @JsonIgnore
+    private String senha;
+
+    @CreationTimestamp
+    private LocalDateTime dataCadastro;
+
+    @UpdateTimestamp
+    private LocalDate dataNascimento;
 
 }
