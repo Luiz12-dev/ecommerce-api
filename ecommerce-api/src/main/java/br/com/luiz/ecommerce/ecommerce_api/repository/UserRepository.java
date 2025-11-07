2b4316bd9e51f4e593173de54ecabb73f55f7a2d
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-07 14:46:58 -0300

    feat:adiciona metodo de busca por email

--- ecommerce-api/src/main/java/br/com/luiz/ecommerce/ecommerce_api/repository/UserRepository.java
+++ ecommerce-api/src/main/java/br/com/luiz/ecommerce/ecommerce_api/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package br.com.luiz.ecommerce.ecommerce_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.luiz.ecommerce.ecommerce_api.domain.Usuario;
 
 public interface UserRepository extends JpaRepository<Usuario, Long> {
 
-
+    Optional<Usuario> findByEmail(String email);
+    
 }
