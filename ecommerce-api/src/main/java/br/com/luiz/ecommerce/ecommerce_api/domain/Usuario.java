7166a4df356a4e59510c4d9d254d0e9f238f79bd
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-10 14:24:36 -0300

    feat(domain): Adiciona anotação @Setter

--- ecommerce-api/src/main/java/br/com/luiz/ecommerce/ecommerce_api/domain/Usuario.java
+++ ecommerce-api/src/main/java/br/com/luiz/ecommerce/ecommerce_api/domain/Usuario.java
@@ -19,10 +19,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "usuarios")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
